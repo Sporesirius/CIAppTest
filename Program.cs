60782519ccb50335eda9d55781663e4a4023dc59
Sporesirius <sporesirius@pm.me>
2020-11-26 19:30:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CIAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
